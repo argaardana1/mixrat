17983acea4eb4aa8c7071cd8f3f0c7ebcbd33fe0
argaardana1 <remajasehat007@gmail.com>
2026-04-10 15:14:36 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      animation: {
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [],
+};
+
+export default config;
